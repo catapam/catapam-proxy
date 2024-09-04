919a215d0c00166f3bb2acc7b3b9492ba2303cce
catapam <matheuscatapam@gmail.com>
2024-09-04 17:53:51 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,19 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+// Proxy requests to /news without adding /news again
+app.use('/news', createProxyMiddleware({
+    target: 'https://catapam.wpengine.com',  // Directly target WP Engine root
+    changeOrigin: true,                      // Keep the correct origin
+    pathRewrite: { '^/news': '' },           // Strip /news from the Heroku request path
+    headers: {                               // Ensure the X-Forwarded-Host is correct
+        'X-Forwarded-Host': 'catapam.wpengine.com',
+    }
+}));
+
 // Fallback route for non-proxied requests
 app.get('*', (req, res) => {
-    res.status(200).send('This is a test');
+    res.status(200).send('This is not in WP Engine');
 });
 
 const PORT = process.env.PORT || 5000;

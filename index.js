2c357eb74ee288a9a65c3863c02b375a7d4c9fd0
catapam <matheuscatapam@gmail.com>
2024-11-14 10:55:34 +0000

    Update env vars

--- index.js
+++ index.js
@@ -3,13 +3,18 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
+// Load environment variables
+const TARGET = process.env.TARGET;
+const FORWARDED_HOST = process.env.FORWARDED_HOST;
+const PATH_REWRITE = process.env.PATH_REWRITE || '^/';
+
 // Proxy requests to /news without adding /news again
 app.use('/news', createProxyMiddleware({
-    target: 'https://catapam.wpengine.com',  // Directly target WP Engine root
-    changeOrigin: true,                      // Keep the correct origin
-    pathRewrite: { '^/news': '' },           // Strip /news from the Heroku request path
+    target: TARGET,                         // Use environment variable for target
+    changeOrigin: true,                     // Keep the correct origin
+    pathRewrite: { [PATH_REWRITE]: '' },    // Use environment variable for pathRewrite
     headers: {                               // Ensure the X-Forwarded-Host is correct
-        'X-Forwarded-Host': 'catapam.wpengine.com',
+        'X-Forwarded-Host': FORWARDED_HOST, // Use environment variable for X-Forwarded-Host
     }
 }));
 
